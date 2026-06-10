37ac17e122af033f4b706d924406d3e387500309
franco <franco.cortez.nunez@gmail.com>
2026-06-10 10:10:08 -0400

    feat(doctor): register doctor child route

--- src/app/pages/maintainer/maintainer.routes.ts
+++ src/app/pages/maintainer/maintainer.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { SchemasComponent } from './schemas/schemas.component';
 import { ActiveIngredientComponent } from './active-ingredient/active-ingredient.component';
 import { CommercialProductComponent } from './commercial-product/commercial-product.component';
+import { DoctorComponent } from './doctor/doctor.component';
 
 export default [
   { path: 'schemas', component: SchemasComponent },
   { path: 'pa', component: ActiveIngredientComponent },
   { path: 'commercial-product', component: CommercialProductComponent },
+  { path: 'doctor', component: DoctorComponent },
 ] as Routes;
